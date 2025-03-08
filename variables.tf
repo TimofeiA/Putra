d46a6585bac46a628d9155b76fb3fef092bfb339
Timothy <divico445@google.com>
2025-03-08 17:32:05 -0500

    Variable file added

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "server_port" {
+  description = "The port that the server will use to handle HTTP request"
+  default     = 8080
+  type        = number
+
+  validation {
+    condition     = var.server_port > 0 && var.server_port < 65536
+    error_message = "The port number must be between 1-65535"
+  }
+
+  sensitive = true
+}
+
